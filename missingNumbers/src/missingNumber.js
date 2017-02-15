66b743134fde425d8d0f6a420a88dff7a799abc6
Omokaro Faith <solomonfaith70@gmail.com>
2017-02-15 14:38:32 +0100

    This folder contains the  missing number program and the test file for testing validity of the program

---
+++ missingNumbers/src/missingNumber.js
@@ -0,0 +1,18 @@
+
+function findMissing (arr1, arr2) {
+    if (arr1.length === 0 && arr2.length === 0){
+        return 0;
+    }
+ var number;
+ for (var i in arr2){
+     if(arr1.length === arr2.length) {
+        return 0;
+    }
+   else if (arr1.indexOf(arr2[i]) == -1){
+     var number = arr2[i];
+   }
+ }
+ return number;
+}
+
+module.exports = findMissing;
